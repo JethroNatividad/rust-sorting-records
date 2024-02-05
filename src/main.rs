0425e372ec6eba14249098321d477e0fbb76b7ab
JethroNatividad <natividad.jet@gmail.com>
2024-02-06 07:37:52 +0800

    write problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+// Create a program that sorts this dataset by last name and print by tabular format.
+// Dataset:
+// First Name Last Name Position Separation date
+// John Johnson Manager 2016-12-31
+// Tou Xiong Software Engineer 2016-10-05
+// Michaela Michaelson District Manager 2015-12-19
+// Jake Jacobson Programmer
+// Jacquelyn Jackson DBA
+// Sally Weber Web Developer 2015-12-18
+
 fn main() {
     println!("Hello, world!");
 }
